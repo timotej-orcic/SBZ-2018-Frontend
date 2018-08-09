13cfd1ddb0b150b9c3b22c6232c9b9cf6044ce16
timotej-orcic <timotej.orcic@gmail.com>
2018-08-09 20:30:10 +0200

    Rutiranje + dizajn register stranice

--- sbz-app/src/app/app.module.ts
+++ sbz-app/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
